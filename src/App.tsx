834eb0928144355c4f5e23bef32c98b36ebd0965
Ashish <ashish74624@gmail.com>
2024-04-15 23:47:53 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+
+
+function App() {
+
+  return (
+    <section className="w-screen h-max min-h-screen bg-[#DDD3C9]/90">
+      <Navbar/>
+      <img className="w-screen" src="/1.jpeg"/>
+    </section>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+export default function Navbar() {
+  return (
+    <nav className='w-screen py-4 bg-white text-blue-800 px-4 text-4xl font-bold shadow'>
+      OpenSolutions
+    </nav>
+  )
+}
